a7347f425a01ec5a11a12d0659012bc5df3c14df
Kevin TANG <Kevin.Tang@etu.sorbonne-universite.fr>
2026-03-23 01:30:30 +0100

    ajout fichier frontend

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+//import './App.css'
+import Anime from './components/Anime'
+import RegisterForm from './components/RegisterForm'
+import LoginForm from './components/LoginForm'
+
+function App() {
+  return (
+
+   <LoginForm></LoginForm>
+
+  )
+}
+
+export default App

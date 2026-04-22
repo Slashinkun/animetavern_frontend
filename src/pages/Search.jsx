bccafad42615844c4221f6d0c68586b425bf047c
Kevin TANG <Kevin.Tang@etu.sorbonne-universite.fr>
2026-04-22 22:52:27 +0200

    implementation search page

---
+++ src/pages/Search.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Search() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const [hasSearched, setHasSearched] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSearch = async () => {
+    if (!query) return;
+
+    try {
+      const res = await fetch(
+        "http://localhost:8080/search?query=" + encodeURIComponent(query)
+      );
+
+      setHasSearched(true);
+      const data = await res.json();
+
+      // Supprime les doublons par mal_id
+      const uniqueAnimes = Array.from(new Map(data.data.map(a => [a.mal_id, a])).values());
+
+      // Filtre pour garder seulement les titres qui contiennent la query
+      const filteredAnimes = uniqueAnimes.filter(anime =>
+        anime.title.toLowerCase().includes(query.toLowerCase())
+      );
+
+      setResults(filteredAnimes);
+    } catch (err) {
+      console.error("Erreur fetch Jikan:", err);
+      setResults([]);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Rechercher un anime..."
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+        className=" border border-gray rounded m-2"
+      />
+      <button onClick={handleSearch}>Chercher</button>
+
+      <div style={{ marginTop: "20px" }}>
+        {hasSearched && results.length === 0 && <p>Aucun résultat</p>}
+
+        {results.map((anime, index) => (
+          <div
+           key={anime.mal_id}
+            style={{ marginBottom: "10px", cursor: "pointer" }}
+            onClick={() => navigate(`/anime/${anime.mal_id}`)}
+          >
+            <img
+              src={anime.images.jpg.small_image_url}
+              alt={anime.title}
+              width={50}
+              style={{ marginRight: "10px" }}
+            />
+            <span>
+              {anime.title_english || anime.title} ({anime.episodes || "?"} eps)
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}

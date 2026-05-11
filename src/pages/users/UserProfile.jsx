6f0a7cf0b8a3ded49ca32d7812733bfd5a32ece6
Kevin TANG <Kevin.Tang@etu.sorbonne-universite.fr>
2026-05-11 19:25:04 +0200

    implementation mise a jour de la liste d'anime apres suppression

--- src/pages/users/UserProfile.jsx
+++ src/pages/users/UserProfile.jsx
@@ -2,16 +2,28 @@
 import { useOutletContext } from "react-router-dom"
 import AnimeEntry from "../../components/AnimeEntry"
 export default function UserProfile(){
-    const { userData } = useOutletContext()
+    const { userData, setUserData } = useOutletContext()
     return (
         <div>
         <h2>Animes:</h2>
         <ul>
           {userData.animes.map(a => (
-            // <li key={a.id}>
-            //   {a.title} ({a.episodes} épisodes)
-            // </li>
-            <AnimeEntry anime={a} isUser={userData.is_user} key={a.mal_id}></AnimeEntry>
+           
+            <AnimeEntry anime={a} 
+            isUser={userData.is_user} 
+            key={a.mal_id}
+            onRemove={(animeId) => {
+              setUserData(prev => ({
+                ...prev,
+                animes: prev.animes.filter(
+                  anime => anime.mal_id != animeId
+                )
+              }))
+            }} />
+
+
+
+            
           ))}
         </ul>
 

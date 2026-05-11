1185cbf74d689a66c53ae2963df5baf2c5f63d46
Kevin TANG <Kevin.Tang@etu.sorbonne-universite.fr>
2026-05-11 19:22:14 +0200

    ajout fonctionnalité retirer anime de la liste

--- src/components/AnimeEntry.jsx
+++ src/components/AnimeEntry.jsx
@@ -1,9 +1,28 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function AnimeEntry({anime, isUser}){
+
+export default function AnimeEntry({anime, isUser, onRemove}){
+
     
     
+    const handleRemove = async () => {
+        try {
+            const res = await fetch(`http://localhost:8080/user/anime/${anime.mal_id}`, {
+                method: "DELETE",
+                credentials: "include"
+                });
+
+            if (!res.ok) {
+            console.error(await res.text());
+            return;
+
+            }
+            onRemove(anime.mal_id);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
     return (
         <div className="flex justify-between items-center border px-2 hover:bg-gray-400 p-2">
@@ -17,8 +36,21 @@ export default function AnimeEntry({anime, isUser}){
                 {isUser && (
                     <div className="flex gap-2">
                     <button className="border w-10">+</button>
-                    <button className="border w-10">-</button>
+                    {anime.viewed_episodes > 0 && (
+                        <button className="border w-10">-</button>
+                    )}
+                    
                 </div>
+                )}
+
+                {isUser && (
+                    <button 
+                className="border p-2 rounded-xl bg-red-600 font-bold hover:bg-red-800 text-white"
+                onClick={handleRemove}
+                
+                >
+                    Remove
+                </button>
                 )}
                 
             </div>

a9bde3f197f94eecf5f5855888366af1eae3edf2
Kevin TANG <Kevin.Tang@etu.sorbonne-universite.fr>
2026-05-14 22:51:28 +0200

    ajout fonctionnalité mise a jour compteur anime et status de visionnage

--- src/components/AnimeEntry.jsx
+++ src/components/AnimeEntry.jsx
@@ -2,20 +2,21 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 
-export default function AnimeEntry({anime, isUser, onRemove}){
+export default function AnimeEntry({ anime, isUser, onRemove, onUpdateEpisodes }) {
 
-    
-    
+    const [isEditing, setEditing] = useState(false)
+    const [status, setStatus] = useState(anime.status);
+
     const handleRemove = async () => {
         try {
             const res = await fetch(`http://localhost:8080/user/anime/${anime.mal_id}`, {
                 method: "DELETE",
                 credentials: "include"
-                });
+            });
 
             if (!res.ok) {
-            console.error(await res.text());
-            return;
+                console.error(await res.text());
+                return;
 
             }
             onRemove(anime.mal_id);
@@ -24,35 +25,139 @@ export default function AnimeEntry({anime, isUser, onRemove}){
         }
     }
 
+
+    const updateEpisodes = async (delta) => {
+        try {
+            const res = await fetch(`http://localhost:8080/user/anime/${anime.mal_id}/episodes`,
+                {
+                    method: "PATCH",
+                    credentials: "include",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ delta })
+                }
+            )
+
+            if (!res.ok) {
+                console.error(await res.text());
+                return;
+            }
+
+            const data = await res.json();
+
+
+            onUpdateEpisodes(anime.mal_id, data);
+
+
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    const updateStatus = async () => {
+        try {
+            const res = await fetch(
+                `http://localhost:8080/user/anime/${anime.mal_id}/status`,
+                {
+                    method: "PATCH",
+                    credentials: "include",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ status })
+                })
+
+            if (!res.ok) {
+                console.error(await res.text());
+                return;
+            }
+
+            onUpdateEpisodes(anime.mal_id, {
+                ...anime,
+                status
+            });
+
+            setEditing(false);
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+
     return (
         <div className="flex justify-between items-center border px-2 hover:bg-gray-400 p-2">
             <div className="flex items-center gap-4">
                 <img src={anime.image_url} alt="" className="w-20 h-25 object-cover rounded p-2" />
                 <Link to={`/anime/${anime.mal_id}`}>
-                <p className="wrap-break-word font-semibold" >{anime.title}</p>
+                    <p className="wrap-break-word font-semibold" >{anime.title}</p>
                 </Link>
                 <span>{anime.viewed_episodes}/{anime.episodes}</span>
-                <p>{anime.status}</p>
+
+                <p>{status}</p>
+
+
+
+                {isEditing ? (
+                    <div>
+                        <select value={status} onChange={(e) => setStatus(e.target.value)}
+                            className="border"
+                        >
+                            <option value="PLANNING">PLANNING</option>
+                            <option value="WATCHING">WATCHING</option>
+                            <option value="FINISHED">FINISHED</option>
+                        </select>
+
+                        <button onClick={updateStatus}
+                            className="border rounded p-1 bg-green-500 hover:bg-green-600"
+                        >
+                            Confirm edit
+                        </button>
+
+                        <button onClick={() => setEditing(false)}
+                            className="border rounded p-1 bg-red-500 hover:bg-red-600"
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                ) : (
+                    <button onClick={() => setEditing(true)}
+                        className="border rounded p-1 bg-green-500 hover:bg-green-600"
+                    >
+                        Edit status
+                    </button>
+                )}
+
+
                 {isUser && (
                     <div className="flex gap-2">
-                    <button className="border w-10">+</button>
-                    {anime.viewed_episodes > 0 && (
-                        <button className="border w-10">-</button>
-                    )}
-                    
-                </div>
+
+                        <button
+                            className="border w-10 disabled:bg-gray-700 hover:bg-gray-500"
+                            onClick={() => updateEpisodes(1)}
+                            disabled={anime.viewed_episodes >= (anime.episodes ?? 0)}
+                        >
+                            +
+                        </button>
+
+                        <button
+                            className="border w-10 disabled:bg-gray-700  hover:bg-gray-500"
+                            onClick={() => updateEpisodes(-1)}
+                            disabled={anime.viewed_episodes <= 0}
+                        >
+                            -
+                        </button>
+
+                    </div>
                 )}
 
                 {isUser && (
-                    <button 
-                className="border p-2 rounded-xl bg-red-600 font-bold hover:bg-red-800 text-white"
-                onClick={handleRemove}
-                
-                >
-                    Remove
-                </button>
+                    <button
+                        className="border p-2 rounded-xl bg-red-600 font-bold hover:bg-red-800 text-white"
+                        onClick={handleRemove}
+
+                    >
+                        Remove
+                    </button>
                 )}
-                
+
             </div>
         </div>
     )
--- src/pages/users/UserProfile.jsx
+++ src/pages/users/UserProfile.jsx
@@ -1,16 +1,16 @@
 
 import { useOutletContext } from "react-router-dom"
 import AnimeEntry from "../../components/AnimeEntry"
-export default function UserProfile(){
-    const { userData, setUserData } = useOutletContext()
-    return (
-        <div>
-        <h2>Animes:</h2>
-        <ul>
-          {userData.animes.map(a => (
-           
-            <AnimeEntry anime={a} 
-            isUser={userData.is_user} 
+export default function UserProfile() {
+  const { userData, setUserData } = useOutletContext()
+  return (
+    <div>
+      <h2>Animes:</h2>
+      <ul>
+        {userData.animes.map(a => (
+
+          <AnimeEntry anime={a}
+            isUser={userData.is_user}
             key={a.mal_id}
             onRemove={(animeId) => {
               setUserData(prev => ({
@@ -19,16 +19,37 @@ export default function UserProfile(){
                   anime => anime.mal_id != animeId
                 )
               }))
-            }} />
+
+            }}
+
+            onUpdateEpisodes={(animeId, data) => {
+              setUserData(prev => ({
+                ...prev,
+                animes: prev.animes.map(anime =>
+                  anime.mal_id === animeId
+                    ? {
+                      ...anime,
+                      viewed_episodes: data.viewed_episodes,
+                      status: data.status
+                    }
+                    : anime
+                )
+              }));
+            }}
 
 
 
-            
-          ))}
-        </ul>
 
-        
+          />
+
+
+
+
+        ))}
+      </ul>
+
+
     </div>
-    )
+  )
 
 }

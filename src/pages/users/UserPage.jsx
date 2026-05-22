8d6b641153c78f07308fc862e617542930eec235
Kevin TANG <Kevin.Tang@etu.sorbonne-universite.fr>
2026-05-22 02:59:01 +0200

    correction4

--- src/pages/users/UserPage.jsx
+++ src/pages/users/UserPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import { useParams, NavLink, Outlet } from "react-router-dom"
 import AnimeEntry from "../../components/AnimeEntry"
-import { API_URL } from ".../utils/config";
+import { API_URL } from "./../utils/config";
 export default function UserPage() {
   const { id } = useParams()
   const [userData, setUserData] = useState(null)
